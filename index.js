82fec8302a876095725d8315e8f55163d412c245
Dakota <dakota.klasky@gmail.com>
2024-07-06 15:03:44 -0400

    DOM manipulation

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
 // Write your code here!
+
+//remove main id
+
+//newHeader variable that points to an <h1> node
+//with id of victory
+//and text Dakota is the champion
+
+const m = document.getElementById("main");
+m.remove();
+
+const newHeader = document.createElement("h1");
+newHeader.id = "victory";
+newHeader.textContent = "DAKOTA KLASKY is the champion";
+document.body.append(newHeader);
+
+
